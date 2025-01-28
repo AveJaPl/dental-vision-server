43df4720c61e96dca4fcf8da50da59bc4dd3837e
AveJaPl <filip.piatek2005@gmail.com>
2025-01-28 17:04:42 +0100

    log decoded token

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
+import logger from '../logger';
 
 const prisma = new PrismaClient();
 
 export const checkAuth = async (token: string): Promise<string | null> => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string };
+        logger.info(`Decoded token: ${JSON.stringify(decoded)}`);
         const user = await prisma.user.findUnique({
             where: { id: decoded.userId },
         });
